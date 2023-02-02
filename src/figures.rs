b6f3240b56e930ef44fbee0b04442b3395d44e40
Darin Chau <darinchauyf@gmail.com>
2023-02-02 13:14:27 -0800

    Base displayable and plot options

--- src/figures.rs
+++ src/figures.rs
@@ -1,1 +1,8 @@
-pub mod utils;
+mod base;
+mod utils;
+
+pub use utils::coordinates::Coordinates;
+pub use base::displayable::Displayable;
+pub use base::plotoptions::Color;
+pub use base::plotoptions::PlotOptions;
+pub use base::plotoptions::TikzColor;
