7661f4f2e0f2f848cba64453952c81bb0f209f3d
Darin Chau <darinchauyf@gmail.com>
2023-02-04 19:34:41 -0800

    add doctest

--- src/figures/drawables/point.rs
+++ src/figures/drawables/point.rs
@@ -2,6 +2,21 @@
 
 use crate::figures::{Coordinates, Drawable, FigureObject, FOPoint};
 
+/// The implementation of a node with no contents.
+/// 
+/// Example
+/// ```
+/// use tikzpaint_rs::figures::{Point, Coordinates, Figure, Identity};
+/// let p1 = Point::new(Coordinates::new([2, 3]));
+/// let p2 = Point::new(Coordinates::new([4, 5]));
+/// let mut fig = Figure::<2>::new();
+/// fig.draw(&p1);
+/// fig.draw(&p2);
+/// 
+/// let result = fig.output_tikz(&Identity);
+/// let expect = "\\begin{tikzpicture}\n\t\\node[] at (2, 3) {}\n\t\\node[] at (4, 5) {}\n\\end{tikzpicture}";
+/// assert_eq!(result, expect);
+/// ```
 pub struct Point<const DIMS: usize> {
     p: FOPoint<DIMS>,
 }
