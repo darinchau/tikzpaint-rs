67355026c696352f585056892335e00f4160f96e
Darin Chau <darinchauyf@gmail.com>
2023-02-08 14:57:47 -0800

    merge sidebar

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
+//! Base module for Tikzpaint-rs. We try to structure the app such that
+//! the "figures" module can also work as a CLI tool for Tikz figure generation
+
 pub mod figures;
 pub mod app;
+
